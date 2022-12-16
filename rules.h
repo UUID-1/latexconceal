3ae685892f5c1e1e78fde827e53931cf75d8aeea
Juiyung <proteus@osboxes>
2022-12-16 15:05:35 +0800

    update license information

--- rules.h
+++ rules.h
@@ -1,6 +1,6 @@
 /*  unitex: TeX-to-Unicode converter.
  *  Copyright (C) 2022 Juiyung Hsu
- *  License: GNU General Public License (version 3 or later)
+ *  License: GNU General Public License v3.0
  *  You should have received a copy of the license along with this
  *  file. If not, see <http://www.gnu.org/licenses>.
  */
